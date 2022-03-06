a29255eaf9b39601718625966f7a8f1368e4abd5
diana <diana@gmail.com>
2022-03-06 20:25:15 +0200

    initial commit

---
+++ Laboratory3/main.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "complex_test.h"
+
+int main(int argc, char** argv) {
+
+	run_complex_tests(true);
+
+	// TODO your code here
+	
+	return 0;
+}
